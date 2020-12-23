f855e8f8a54f235f1f4d65788fa392f38db6635d
josuecedeno <josuecedeno@yahoo.com>
2020-12-23 13:19:09 -0600

    JS Updates for selections

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ let answerB = document.getElementById('b');
 let answerC = document.getElementById('c');
 let answerD = document.getElementById('d');
 
+let answers = document.querySelectorAll('.answer');
 let answer;
 
+let question = document.getElementById('question');
 let result = document.getElementById('result');
 
+let message = document.getElementById('message');
 let next = document.getElementById('next');
 
 const setQuestion = () => {
@@ -29,34 +32,89 @@ const setQuestion = () => {
   // How many questions
   totalIds.innerText = quizQuestions.length;
 
-  // Assign first question
+  // Assign question
   questionText.innerText = quizQuestions[id].question;
-  answerA.innerText = quizQuestions[id].choices['a'];
-  answerB.innerText = quizQuestions[id].choices['b'];
-  answerC.innerText = quizQuestions[id].choices['c'];
-  answerD.innerText = quizQuestions[id].choices['d'];
+  answerA.innerText = `a. ${quizQuestions[id].choices['a']}`;
+  answerB.innerText = `b. ${quizQuestions[id].choices['b']}`;
+  answerC.innerText = `c. ${quizQuestions[id].choices['c']}`;
+  answerD.innerText = `d. ${quizQuestions[id].choices['d']}`;
 
   // Hide result section
+  showClear(false);
   showResult(false);
+  enableAnswers(true);
 };
 
 const checkAnswer = (e) => {
   let selection = e.target.id;
 
   if (selection === answer) {
-    console.log('Nice Job!');
-    showResult(true);
+    message.innerText = 'Well Done! Correct!';
+    score++;
+    showMessage(true);
+    showNext(true);
+    showClear(false);
   } else {
     console.log('Try again.');
+    message.innerText = `Sorry. The answer is ${answer}.`;
+    showMessage(true);
+    showNext(true);
+    showClear(false);
+  }
+
+  enableAnswers(false);
+  showResult(true);
+};
+
+// ENABLE/DISABLE THE ANSWER BUTTONS
+const enableAnswers = (enable) => {
+  answers.forEach((a) => {
+    if (!enable) {
+      a.classList.add('no-click');
+    } else {
+      a.classList.remove('no-click');
+    }
+  });
+};
+
+// SHOW / HIDE BUTTONS
+const showMessage = (show) => {
+  if (show) {
+    message.style.display = 'block';
+  } else {
+    message.style.display = 'none';
+  }
+};
+
+const showNext = (show) => {
+  if (show) {
+    next.style.display = 'block';
+  } else {
+    next.style.display = 'none';
+  }
+};
+
+const showClear = (show) => {
+  if (show) {
+    clear.style.display = 'block';
+  } else {
+    clear.style.display = 'none';
   }
 };
 
 const showResult = (show) => {
-  // hide / hide result section
   if (show) {
     result.style.display = 'block';
   } else {
     result.style.display = 'none';
+  }
+};
+
+const showQuestion = (show) => {
+  if (show) {
+    question.style.display = 'block';
+  } else {
+    question.style.display = 'none';
   }
 };
 
@@ -70,8 +128,24 @@ next.addEventListener('click', () => {
   if (id < quizQuestions.length - 1) {
     id += 1;
     console.log('Current Question: ', id + 1);
+    setQuestion();
+  } else {
+    console.log('Show the score and Clear');
+    showQuestion(false);
+
+    message.innerText = `You answered ${score} out of ${quizQuestions.length} correctly.`;
+    showNext(false);
+    showClear(true);
+    showResult(true);
   }
+});
+
+// Restart the quiz
+clear.addEventListener('click', () => {
+  id = 0;
+  score = 0;
   setQuestion();
+  showQuestion(true);
 });
 
 setQuestion(); // initialize the game
